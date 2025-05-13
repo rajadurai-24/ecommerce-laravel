ac8b3e676359fc1d35046ffe279da2e775e03358
rajadurai-24 <sri006283@gmail.com>
2025-05-13 15:52:19 +0530

    backend-created

---
+++ app/Livewire/ProductsPage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+
+#[Title('Products - DCodeMania')]
+class ProductsPage extends Component{
+
+    use WithPagination;
+
+    public function render()
+    {
+        $productQuery = Product::query()->where('is_active',1);
+        return view('livewire.products-page',[
+            'products' => $productQuery->paginate(6),
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckOutPage;
+use App\Livewire\HomePage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\MyOrderPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
+use Filament\Pages\Auth\Register;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', HomePage::class);
+Route::get('/categories',CategoriesPage::class);
+Route::get('/products',ProductsPage::class);
+Route::get('/cart',CartPage::class);
+Route::get('/products/{product}',ProductDetailPage::class);
+
+Route::get('/checkout', CheckOutPage::class);
+Route::get('/myorderpage',MyOrderPage::class);
+Route::get('/myorderdetailpage', MyOrderDetailPage::class);
+
+Route::get('login',LoginPage::class);
+Route::get('/register',RegisterPage::class);
+Route::get('/forgot',ForgotPasswordPage::class);
+Route::get('/reset',ResetPasswordPage::class);
+
+Route::get('/success',SuccessPage::class);
+Route::get('/cancel',CancelPage::class);
